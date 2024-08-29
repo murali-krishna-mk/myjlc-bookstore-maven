c1f98f7320045200f778f5f144ffa1803491078d
murali-krishna-mk <pulimuralikrishna.mk@gmail.com>
2024-08-29 14:39:44 +0530

    Update - 6 by Murali

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -13,6 +13,7 @@ System.out.println("Update - 2 by Murali");
 System.out.println("Update - 3 by Murali");
 System.out.println("Update - 4 by Murali");
 System.out.println("Update - 5 by Murali");
+System.out.println("Update - 6 by Murali");
 
 SpringApplication.run(MyBootApplication.class, args);
 }
